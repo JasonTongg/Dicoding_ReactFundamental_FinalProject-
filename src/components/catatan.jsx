8302f023caa7d7507beed086b0ca3917d0d952a5
JasonTongg <aboytong123@gmail.com>
2022-08-05 16:08:28 +0700

    add empty text in note

--- src/components/catatan.jsx
+++ src/components/catatan.jsx
@@ -9,11 +9,25 @@ export default function catatan(props) {
     <div className='containerCatatan'>
       <h2>{props.judul}</h2>
       <div className="containerCatatanItems">
-        {props.judul === "Catatan Aktif" ? props.aktif.map(item => (
-          <CatatanItem judul={item.title} date={showFormattedDate(date)} catatan={item.body} button="Arsipkan" key={item.id} id={item.id} type="Aktif" setArsip={props.setArsip} arsip={props.arsip} setAktif={props.setAktif} aktif={props.aktif} Aktif={props.Aktif} Arsip={props.Arsip} SetAktif={props.SetAktif} SetArsip={props.SetArsip}></CatatanItem>
-        )) : props.arsip.map(item => (
-          <CatatanItem judul={item.title} date={showFormattedDate(date)} catatan={item.body} button="Pindahkan" key={item.id} id={item.id} type="Arsip" setArsip={props.setArsip} arsip={props.arsip} setAktif={props.setAktif} aktif={props.aktif} Aktif={props.Aktif} Arsip={props.Arsip} SetAktif={props.SetAktif} SetArsip={props.SetArsip}></CatatanItem>
-        ))}
+        {props.judul === "Catatan Aktif" 
+        
+        ? (props.aktif.length !== 0 
+          
+          ? props.aktif.map(item => (
+            <CatatanItem judul={item.title} date={showFormattedDate(date)} catatan={item.body} button="Arsipkan" key={item.id} id={item.id} type="Aktif" setArsip={props.setArsip} arsip={props.arsip} setAktif={props.setAktif} aktif={props.aktif} Aktif={props.Aktif} Arsip={props.Arsip} SetAktif={props.SetAktif} SetArsip={props.SetArsip}></CatatanItem>
+          )) 
+        
+          : <h3>Tidak ada catatan</h3>) 
+        
+        : (props.arsip.length !== 0 
+        
+          ? props.arsip.map(item => (
+            <CatatanItem judul={item.title} date={showFormattedDate(date)} catatan={item.body} button="Pindahkan" key={item.id} id={item.id} type="Arsip" setArsip={props.setArsip} arsip={props.arsip} setAktif={props.setAktif} aktif={props.aktif} Aktif={props.Aktif} Arsip={props.Arsip} SetAktif={props.SetAktif} SetArsip={props.SetArsip}></CatatanItem>
+          )) 
+          
+          : <h3>Tidak ada catatan</h3>)
+        
+        }
       </div>
     </div>
   )
